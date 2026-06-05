2e9600edd1d3ea1c6320e8a72358e68215a37fd9
magjan19 <magda.kasia21@gmail.com>
2026-06-05 19:55:04 +0200

    Dodałam klasę Direction
    Tę klasę dodałam do pliku snake.py i zmieniłam kod, aby był powiązany z tą klasą. Dodałam też do klasy Snake checkSelfHit i zmieniłam self.cialo.

--- snake.py
+++ snake.py
@@ -1,15 +1,25 @@
+from enum import Enum
+
+class Direction(Enum):
+    UP = 1
+    DOWN = 2
+    LEFT = 3
+    RIGHT = 4
+
 class Segment:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def __eq__(self, inny):
+        if not isinstance(inny, Segment):
+            return False
         return self.x == inny.x and self.y == inny.y
 
 class Snake:
     def __init__(self, x, y, rozmiar_bloku=20):
-        self.cialo = [Segment(x, y)]
-        self.kierunek = "PRAWO" 
+        self.cialo = [Segment(x, y), Segment(x - rozmiar_bloku, y)]
+        self.kierunek = Direction.RIGHT 
         self.rozmiar_bloku = rozmiar_bloku
         self.wynik = 0
         self.predkosc = 10
@@ -20,13 +30,13 @@ class Snake:
 
     def sterowanie(self):
         nowy_x, nowy_y = self.glowa.x, self.glowa.y
-        if self.kierunek == "GORA":
+        if self.kierunek == Direction.UP:
             nowy_y -= self.rozmiar_bloku
-        elif self.kierunek == "DOL":
+        elif self.kierunek == Direction.DOWN:
             nowy_y += self.rozmiar_bloku
-        elif self.kierunek == "LEWO":
+        elif self.kierunek == Direction.LEFT:
             nowy_x -= self.rozmiar_bloku
-        elif self.kierunek == "PRAWO":
+        elif self.kierunek == Direction.RIGHT:
             nowy_x += self.rozmiar_bloku
 
         self.cialo.insert(0, Segment(nowy_x, nowy_y))
@@ -35,13 +45,13 @@ class Snake:
     def rosnij(self, bonus_predkosci):
         nowy_x, nowy_y = self.glowa.x, self.glowa.y
 
-        if self.kierunek == "GORA":
+        if self.kierunek == Direction.UP:
             nowy_y -= self.rozmiar_bloku
-        elif self.kierunek == "DOL":
+        elif self.kierunek == Direction.DOWN:
             nowy_y += self.rozmiar_bloku
-        elif self.kierunek == "LEWO":
+        elif self.kierunek == Direction.LEFT:
             nowy_x -= self.rozmiar_bloku
-        elif self.kierunek == "PRAWO":
+        elif self.kierunek == Direction.RIGHT:
             nowy_x += self.rozmiar_bloku
 
         self.cialo.insert(0, Segment(nowy_x, nowy_y))
@@ -49,25 +59,26 @@ class Snake:
         self.predkosc += bonus_predkosci
 
     def zmien_kierunek(self, nowy_kierunek):
-        if nowy_kierunek == "GORA" and self.kierunek != "DOL":
+        if nowy_kierunek == Direction.UP and self.kierunek != Direction.DOWN:
             self.kierunek = nowy_kierunek
-        elif nowy_kierunek == "DOL" and self.kierunek != "GORA":
+        elif nowy_kierunek == Direction.DOWN and self.kierunek != Direction.UP:
             self.kierunek = nowy_kierunek
-        elif nowy_kierunek == "LEWO" and self.kierunek != "PRAWO":
+        elif nowy_kierunek == Direction.LEFT and self.kierunek != Direction.RIGHT:
             self.kierunek = nowy_kierunek
-        elif nowy_kierunek == "PRAWO" and self.kierunek != "LEWO":
+        elif nowy_kierunek == Direction.RIGHT and self.kierunek != Direction.LEFT:
             self.kierunek = nowy_kierunek
 
+    def checkSelfHit(self):
+        return self.glowa in self.cialo[1:]
 
-import pygame
 
 class InputHandler:
     def __init__(self, klawisz_gora, klawisz_dol, klawisz_lewo, klawisz_prawo):
         self.klawisze = {
-            klawisz_gora: "GORA",
-            klawisz_dol: "DOL",
-            klawisz_lewo: "LEWO",
-            klawisz_prawo: "PRAWO"
+            klawisz_gora: Direction.UP,
+            klawisz_dol: Direction.DOWN,
+            klawisz_lewo: Direction.LEFT,
+            klawisz_prawo: Direction.RIGHT
         }
 
     def obsluz_wejscie(self, waz, wcisniety_klawisz):
