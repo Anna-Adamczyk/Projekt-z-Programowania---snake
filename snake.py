6e487ba787e3b11c3de05d4a4dff9ed937073afd
magjan19 <magda.kasia21@gmail.com>
2026-06-08 21:18:10 +0200

    Modyfikacja klasy InputHandler
    Dodałam też do klasy Snake activateBoost i deactivateBoost

--- snake.py
+++ snake.py
@@ -7,7 +7,7 @@ class Direction(Enum):
     RIGHT = 4
 
 class Segment:
-    def __init__(self, x, y):
+    def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
 
@@ -16,13 +16,19 @@ class Segment:
             return False
         return self.x == inny.x and self.y == inny.y
 
+    def getPosition(self):
+        return self.x, self.y
+
 class Snake:
-    def __init__(self, x, y, rozmiar_bloku=20):
-        self.body = [Segment(x, y), Segment(x - rozmiar_bloku, y)]
-        self.direction = Direction.RIGHT 
-        self.rozmiar_bloku = rozmiar_bloku
+    def __init__(self, x: int, y: int, player_id: int = 1, color: str = "Neon"):
+        self.body = [Segment(x, y), Segment(x - 1, y)]
+        self.direction = Direction.RIGHT
         self.score = 0
         self.speed = 10
+        self.color = color
+        self.playerId = player_id
+        self.boostActive = False
+        self.boostDuration = 0
 
     @property
     def glowa(self):
@@ -31,34 +37,35 @@ class Snake:
     def move(self):
         nowy_x, nowy_y = self.glowa.x, self.glowa.y
         if self.direction == Direction.UP:
-            nowy_y -= self.rozmiar_bloku
+            nowy_y -= 1
         elif self.direction == Direction.DOWN:
-            nowy_y += self.rozmiar_bloku
+            nowy_y += 1
         elif self.direction == Direction.LEFT:
-            nowy_x -= self.rozmiar_bloku
+            nowy_x -= 1
         elif self.direction == Direction.RIGHT:
-            nowy_x += self.rozmiar_bloku
+            nowy_x += 1
 
         self.body.insert(0, Segment(nowy_x, nowy_y))
         self.body.pop()
 
-    def grow(self, bonus_predkosci):
+    def grow(self, bonus_predkosci: int = 1):
         nowy_x, nowy_y = self.glowa.x, self.glowa.y
 
         if self.direction == Direction.UP:
-            nowy_y -= self.rozmiar_bloku
+            nowy_y -= 1
         elif self.direction == Direction.DOWN:
-            nowy_y += self.rozmiar_bloku
+            nowy_y += 1
         elif self.direction == Direction.LEFT:
-            nowy_x -= self.rozmiar_bloku
+            nowy_x -= 1
         elif self.direction == Direction.RIGHT:
-            nowy_x += self.rozmiar_bloku
+            nowy_x += 1
 
         self.body.insert(0, Segment(nowy_x, nowy_y))
         self.score += 1
-        self.speed += bonus_predkosci
+        if self.boostActive:
+            self.speed += bonus_predkosci
 
-    def zmien_kierunek(self, nowy_kierunek):
+    def zmien_kierunek(self, nowy_kierunek: Direction):
         if nowy_kierunek == Direction.UP and self.direction != Direction.DOWN:
             self.direction = nowy_kierunek
         elif nowy_kierunek == Direction.DOWN and self.direction != Direction.UP:
@@ -68,20 +75,28 @@ class Snake:
         elif nowy_kierunek == Direction.RIGHT and self.direction != Direction.LEFT:
             self.direction = nowy_kierunek
 
-    def checkSelfHit(self):
+    def checkSelfHit(self) -> bool:
         return self.glowa in self.body[1:]
 
+    def activateBoost(self, duration: int):
+        self.boostActive = True
+        self.boostDuration = duration
+
+    def deactivateBoost(self):
+        self.boostActive = False
+        self.boostDuration = 0
 
 class InputHandler:
-    def __init__(self, klawisz_gora, klawisz_dol, klawisz_lewo, klawisz_prawo):
-        self.klawisze = {
-            klawisz_gora: Direction.UP,
-            klawisz_dol: Direction.DOWN,
-            klawisz_lewo: Direction.LEFT,
-            klawisz_prawo: Direction.RIGHT
-        }
+    def __init__(self, mapping: dict):
+        self.klawisze = mapping
+        self.keyPressed = None
 
-    def obsluz_wejscie(self, waz, wcisniety_klawisz):
-        if wcisniety_klawisz in self.klawisze:
-            nowy_kierunek = self.klawisze[wcisniety_klawisz]
-            waz.zmien_kierunek(nowy_kierunek)
+    def readInput(self, event_key):
+        if event_key in self.klawisze:
+            self.keyPressed = self.klawisze[event_key]
+        else:
+            self.keyPressed = None
+
+    def sendDirection(self, snakes: list):
+        if self.keyPressed and len(snakes) > 0:
+            snakes[0].zmien_kierunek(self.keyPressed)
